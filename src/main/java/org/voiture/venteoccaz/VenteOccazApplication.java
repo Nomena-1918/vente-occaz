2597698fa4bf3ac14d09ca33d112393e91cc7753
nomena <vahatra.nomena@yahoo.com>
2024-01-05 16:41:30 +0300

    c

--- src/main/java/org/voiture/venteoccaz/VenteOccazApplication.java
+++ src/main/java/org/voiture/venteoccaz/VenteOccazApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VenteOccazApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VenteOccazApplication.class, args);
     }
-
 }
--- src/test/java/org/voiture/venteoccaz/VenteOccazApplicationTests.java
+++ src/test/java/org/voiture/venteoccaz/VenteOccazApplicationTests.java
@@ -8,6 +8,7 @@ class VenteOccazApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Hello World !");
     }
 
 }
