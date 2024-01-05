0849bb318f974268cc604cb0886cf7265b54ff83
nomena <vahatra.nomena@yahoo.com>
2024-01-05 11:55:51 +0300

    init

---
+++ src/test/java/org/voiture/venteoccaz/VenteOccazApplicationTests.java
@@ -0,0 +1,13 @@
+package org.voiture.venteoccaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenteOccazApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
