04135f8cd61ea8c7d8d86639253b3b4896d1c96b
Nomena4 <119030942+Nomena-1918@users.noreply.github.com>
2024-01-09 06:49:33 +0300

    LOG !

--- src/main/java/org/voiture/venteoccaz/controllers/AuthController.java
+++ src/main/java/org/voiture/venteoccaz/controllers/AuthController.java
@@ -1,4 +1,65 @@
 package org.voiture.venteoccaz.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.voiture.venteoccaz.Reponse.Reponse;
+
+import org.voiture.venteoccaz.models.Utilisateur;
+import org.voiture.venteoccaz.services.AuthService;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/annonces")
 public class AuthController {
+    private final AuthService authService;
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login-admin")
+    public ResponseEntity<Reponse> LoginAdmin(@RequestBody Utilisateur utilisateur) throws NoSuchAlgorithmException, InvalidKeyException {
+        Optional<Utilisateur> user = authService.isRegisteredAdmin(utilisateur.getEmail(), utilisateur.getMotDePasse());
+        return getReponseResponseEntity(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Reponse> Login(@RequestBody Utilisateur utilisateur) throws NoSuchAlgorithmException, InvalidKeyException {
+        Optional<Utilisateur> user = authService.isRegistered(utilisateur.getEmail(), utilisateur.getMotDePasse());
+        return getReponseResponseEntity(user);
+    }
+
+    private ResponseEntity<Reponse> getReponseResponseEntity(Optional<Utilisateur> utilisateur) throws NoSuchAlgorithmException, InvalidKeyException {
+        Reponse reponse = authService.authenticate(utilisateur);
+        HttpStatus httpStatus = switch (reponse.getCode()) {
+            case "200" -> HttpStatus.OK;
+            case "403" -> HttpStatus.FORBIDDEN;
+            default -> HttpStatus.UNAUTHORIZED;
+        };
+        return new ResponseEntity<>(reponse, httpStatus);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> Logout(@RequestHeader Map<String,String> headers) {
+        try {
+            if (authService.validateAuthorization(headers)) {
+                String token = headers.get("authorization");
+                token = token.replace("Bearer ", "");
+                authService.expireToken(token);
+                return new ResponseEntity<>("Logged out !", HttpStatus.OK);
+            } else
+                return new ResponseEntity<>("Token expiré", HttpStatus.UNAUTHORIZED);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 }
--- src/main/java/org/voiture/venteoccaz/services/AuthService.java
+++ src/main/java/org/voiture/venteoccaz/services/AuthService.java
@@ -3,19 +3,22 @@ package org.voiture.venteoccaz.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.voiture.venteoccaz.Reponse.Reponse;
 import org.voiture.venteoccaz.Repositories.SessionRepository;
 import org.voiture.venteoccaz.Repositories.UtilisateurRepository;
 import org.voiture.venteoccaz.models.Session;
 import org.voiture.venteoccaz.models.Utilisateur;
+import org.voiture.venteoccaz.util.CodeGenerator;
+import org.voiture.venteoccaz.util.TokenGenerator;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class AuthService {
-
-    @Value("${custom.token.duration}")
-    private int tokenDuration;
     private final UtilisateurRepository utilisateurRepository;
     private final SessionRepository sessionRepository;
 
@@ -25,7 +28,7 @@ public class AuthService {
         this.sessionRepository = sessionRepository;
     }
 
-    public Optional<Utilisateur> isRegistered(String email, String motDePasse) {
+    public Optional<Utilisateur> isRegisteredAdmin(String email, String motDePasse) {
         Optional<Utilisateur> utilisateur = utilisateurRepository.findAllByEmailAndMotDePasse(email, motDePasse);
         if (utilisateur.isPresent() && utilisateur.get().getIsAdmin() == 1) {
             return utilisateur;
@@ -33,16 +36,64 @@ public class AuthService {
         return Optional.empty();
     }
 
+    public Optional<Utilisateur> isRegistered(String email, String motDePasse) {
+        return utilisateurRepository.findAllByEmailAndMotDePasse(email, motDePasse);
+    }
+
     public Optional<Session> getSessionWithToken(String token) {
         return sessionRepository.findByToken(token.replace("Bearer ", ""));
     }
-
-
 
     public Boolean validateAuthorization(Map<String,String> headers) {
         String token = headers.get("authorization");
         return token != null && getSessionWithToken(token).isPresent();
     }
 
+    public void expireToken(String token) {
+        Optional<Session> session = getSessionWithToken(token.replace("Bearer ", ""));
+        if (session.isPresent()) {
+            Session s = session.get();
+            s.setIsConnected(0);
+            sessionRepository.save(s);
+        }
+        else
+            throw new RuntimeException("- Token not found -");
+    }
+
+    public Optional<Session> getSessionFromUtilisateur(Utilisateur utilisateur) {
+        return sessionRepository.findAllByUtilisateur(utilisateur.getIdUtilisateur());
+    }
+
+    public Reponse authenticate(Optional<Utilisateur> utilisateur) throws NoSuchAlgorithmException, InvalidKeyException {
+        if (utilisateur.isEmpty()) {
+            return new Reponse("403", "Utilisateur absent de la base de donnée");
+        }
+        Optional<Session> session = getSessionFromUtilisateur(utilisateur.get());
+        Session s;
+        if (session.isPresent()) {
+            s = setSessionActif(session.get(), utilisateur.get());
+        }
+        else {
+            s = new Session();
+            s.setUtilisateur(utilisateur.get());
+            s = sessionRepository.save(setSessionActif(s, utilisateur.get()));
+        }
+
+        return new Reponse("200", "Session de l'utilisateur", s);
+    }
+
+    private Session setSessionActif(Session s, Utilisateur utilisateur) throws NoSuchAlgorithmException, InvalidKeyException {
+        s.setIsConnected(1);
+        s.setDateHeureLogin(LocalDateTime.now());
+        s.setToken(TokenGenerator.getToken(utilisateur.getEmail()));
+
+        String code = CodeGenerator.getCode();
+        while (sessionRepository.findAllByCodeConnected(code).isPresent()) {
+            code = CodeGenerator.getCode();
+        }
+        s.setCode(code);
+        
+        return s;
+    }
 
 }
