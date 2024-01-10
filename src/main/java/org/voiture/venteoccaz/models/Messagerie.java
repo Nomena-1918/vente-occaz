62775238303e9ea310ee5e51d7c2610f414d3754
Nomena-1918 <119030942+Nomena-1918@users.noreply.github.com>
2024-01-11 02:19:23 +0300

    config mongodb

--- src/main/java/org/voiture/venteoccaz/models/Messagerie.java
+++ src/main/java/org/voiture/venteoccaz/models/Messagerie.java
@@ -7,8 +7,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection="messageries")
 public class Messagerie {
 
     @Id
